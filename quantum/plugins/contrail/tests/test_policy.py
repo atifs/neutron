725233f2ab892739ce71031972928bde7a09f1b1
Hampapur Ajay <ajayhn@contrailsystems.com>
2012-10-25 14:32:16 +0000

    add test for inter-vn conn thru policy

--- quantum/plugins/contrail/tests/test_policy.py
+++ quantum/plugins/contrail/tests/test_policy.py
@@ -23,16 +23,50 @@ print "Creating network VN1"
 net_req = {'name': 'vn1'}
 net_rsp = quantum.create_network({'network': net_req})
 net1_id = net_rsp['network']['id']
+net1_fqname = net_rsp['network']['contrail:fq_name']
+net1_fqname_str = ':'.join(net1_fqname)
 
 print "Creating network VN2"
 net_req = {'name': 'vn2'}
 net_rsp = quantum.create_network({'network': net_req})
 net2_id = net_rsp['network']['id']
+net2_fqname = net_rsp['network']['contrail:fq_name']
+net2_fqname_str = ':'.join(net2_fqname)
+
+print "Creating IPAM ipam1"
+dhcp_options = [{'dhcp_option_name': 'opt1', 'dhcp_option_value': 'opt1_value'},
+                      {'dhcp_option_name': 'opt2', 'dhcp_option_value': 'opt2_value'}]
+dhcp_options_list =  DhcpOptionsListType(dhcp_options)
+ipam_mgmt = IpamType.factory(ipam_method = 'dhcp',
+                             dhcp_option_list = dhcp_options_list)
+ipam_mgmt_dict = \
+    json.loads(json.dumps(ipam_mgmt,
+                    default=lambda o: {k:v for k, v in o.__dict__.iteritems()}))
+ipam_req = {'name': 'ipam1',
+            'mgmt': ipam_mgmt_dict
+           }
+ipam_rsp = quantum.create_ipam({'ipam': ipam_req})
+ipam1_fq_name = ipam_rsp['ipam']['fq_name']
+ipam1_id = ipam_rsp['ipam']['id']
+
+print "Creating subnet 10.1.1.0/24 linking to VN1-IPAM1"
+subnet_req = {'network_id': net1_id,
+              'cidr': '10.1.1.0/24',
+              'ip_version': 4,
+              'contrail:ipam_fq_name': ipam1_fq_name}
+subnet_rsp = quantum.create_subnet({'subnet': subnet_req})
+
+print "Creating subnet 20.1.1.0/24 linking to VN2-IPAM1"
+subnet_req = {'network_id': net2_id,
+              'cidr': '20.1.1.0/24',
+              'ip_version': 4,
+              'contrail:ipam_fq_name': ipam1_fq_name}
+subnet_rsp = quantum.create_subnet({'subnet': subnet_req})
 
 print "Creating policy pol1"
 np_rules = [PolicyRuleType(None, '<>', 'pass', 'any',
                 AddressType(virtual_network = ['local']), [PortType(-1, -1)], None,
-                AddressType(virtual_network = ['vn2']), [PortType(-1, -1)], None)]
+                AddressType(virtual_network = [net2_fqname_str]), [PortType(-1, -1)], None)]
 pol_entries = PolicyEntriesType(np_rules)
 pol_entries_dict = \
     json.loads(json.dumps(pol_entries,
@@ -46,7 +80,7 @@ policy1_fq_name = policy_rsp['policy']['fq_name']
 print "Creating policy pol2"
 np_rules = [PolicyRuleType(None, '<>', 'pass', 'any',
                 AddressType(virtual_network = ['local']), [PortType(-1, -1)], None,
-                AddressType(virtual_network = ['vn1']), [PortType(-1, -1)], None)]
+                AddressType(virtual_network = [net1_fqname_str]), [PortType(-1, -1)], None)]
 pol_entries = PolicyEntriesType(np_rules)
 pol_entries_dict = \
     json.loads(json.dumps(pol_entries,
