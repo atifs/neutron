b26ca39b0525320a4d4defbd033a233b20a37a46
Hampapur Ajay <ajayhn@contrailsystems.com>
2012-12-01 16:54:47 -0800

    vars right ini section as wanted by devstack

--- quantum/plugins/contrail/ContrailPlugin.py
+++ quantum/plugins/contrail/ContrailPlugin.py
@@ -72,8 +72,8 @@ class ContrailPlugin(db_base_plugin_v2.QuantumDbPluginV2,
 	"""
 	if cls._cfgdb is None:
             # Initialize connection to DB and add default entries
-            cls._cfgdb = ctdb.config_db.DBInterface(cfg.CONF.vnc.api_server_ip,
-                                                    cfg.CONF.vnc.api_server_port)
+            cls._cfgdb = ctdb.config_db.DBInterface(cfg.CONF.APISERVER.api_server_ip,
+                                                    cfg.CONF.APISERVER.api_server_port)
             # TODO Treat the 2 DBs as logically separate? (same backend for now)
             cls._operdb = cls._cfgdb
             
@@ -108,7 +108,7 @@ class ContrailPlugin(db_base_plugin_v2.QuantumDbPluginV2,
     #end _tenant_list_from_keystone
 
     def __init__(self):
-        cfg.CONF.register_opts(vnc_opts, 'vnc')
+        cfg.CONF.register_opts(vnc_opts, 'APISERVER')
 
         cfg_parser = ConfigParser.ConfigParser()
         ContrailPlugin._parse_class_args(cfg_parser)
