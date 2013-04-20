68d3a8e5eaea658955677a00ee61a8f67a5e6e58
Hampapur Ajay <ajayhn@contrailsystems.com>
2013-04-20 15:57:17 -0700

    default backrefs to None if they don't exist

--- quantum/plugins/contrail/ctdb/config_db.py
+++ quantum/plugins/contrail/ctdb/config_db.py
@@ -500,7 +500,7 @@ class DBInterface(object):
         ipam_q_dict['tenant_id'] = self.manager.tenant_name_to_id(ipam_q_dict.pop('parent_name'))
         ipam_q_dict['fq_name'] = ipam_q_dict.pop('_fq_name')
         ipam_q_dict['mgmt'] = ipam_q_dict.pop('_network_ipam_mgmt', None)
-        net_back_refs = ipam_q_dict.pop('_virtual_network_back_refs')
+        net_back_refs = ipam_q_dict.pop('_virtual_network_back_refs', None)
         if net_back_refs:
             ipam_q_dict['nets_using'] = []
             for net_back_ref in net_back_refs:
@@ -536,7 +536,7 @@ class DBInterface(object):
         policy_q_dict['fq_name'] = policy_q_dict.pop('_fq_name')
         policy_q_dict['tenant_id'] = self.manager.tenant_name_to_id(policy_q_dict.pop('parent_name'))
         policy_q_dict['entries'] = policy_q_dict.pop('_network_policy_entries', None)
-        net_back_refs = policy_q_dict.pop('_virtual_network_back_refs')
+        net_back_refs = policy_q_dict.pop('_virtual_network_back_refs', None)
         if net_back_refs:
             policy_q_dict['nets_using'] = []
             for net_back_ref in net_back_refs:
